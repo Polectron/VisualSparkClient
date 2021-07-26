b225a36c6bb9556da4785c6fbb1500c1c9b20b65
polectron <polectron@gmail.com>
2021-07-26 22:34:28 +0200

    add counter output

---
+++ src/View/Outputs/CounterOutput.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+class CounterOutput extends React.Component<any, any> {
+    private options: any;
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            value: 0
+        }
+    }
+
+
+
+    setCount = (count: number) => {
+        this.setState({value: count});
+    }
+
+    clear = () => {
+        this.setState({value: 0});
+    }
+
+    render() {
+        return (
+            <div className={"elevation"}>
+                <h5>Counter {this.props.id}</h5>
+                {this.state.value}
+            </div>
+        );
+    }
+}
+
+export default CounterOutput;
